933d72fa8add36aa9484f3c97a34b8f2d57863ae
Ale Duarte <duartemattias4@gmail.com>
2024-10-21 19:59:13 -0400

    agregando pipe de control de errores

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
-import { CreateProviderDto } from './dto/create-provider.dto';
-import { UpdateProviderDto } from './dto/update-provider.dto';
-import { NATS_SERVICE } from 'src/config';
-import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
+
+import { NATS_SERVICE } from 'src/config';
+
+import { UpdateProviderDto } from './dto/update-provider.dto';
+import { CreateProviderDto } from './dto/create-provider.dto';
 
 @Controller('providers')
 export class ProvidersController {
@@ -14,28 +16,39 @@ export class ProvidersController {
   @Post()
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.client.send("createProvider", createProviderDto)
-      .pipe( 
-        catchError( error => { throw new RpcException(error) })
-      )    
+      .pipe(
+        catchError(error => { throw new RpcException(error) })
+      )
   }
 
   @Get()
   findAll() {
-    return this.client.send("findAllProviders", {});
+    return this.client.send("findAllProviders", {}).pipe(
+      catchError(error => { throw new RpcException(error) })
+    )
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.client.send("findOneProvider",+id);
+    return this.client.send("findOneProvider", +id)
+      .pipe(
+        catchError(error => { throw new RpcException(error) })
+      )
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
-    return this.client.send("updateProvider",{ id: +id, updateProviderDto});
+    return this.client.send("updateProvider", { id: +id, updateProviderDto })
+      .pipe(
+        catchError(error => { throw new RpcException(error) })
+      )
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.client.send("removeProvider", +id);
+    return this.client.send("removeProvider", +id)
+      .pipe(
+        catchError(error => { throw new RpcException(error) })
+      )
   }
 }
