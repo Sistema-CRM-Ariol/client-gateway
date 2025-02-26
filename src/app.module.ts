6d794680ed10706b83d8a16bcfb36157835c8aad
Mattias Duarte <duartemattias3@gmail.com>
2025-02-26 17:00:20 -0400

    refactorizando el app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule } from './clients/clients.module';
-import { NatsModule } from './transports/nats.module';
-import { ProductsModule } from './products/products.module';
-import { BrandsModule } from './brands/brands.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProvidersModule } from './providers/providers.module';
 import { AuthModule } from './auth/auth.module';
+import { BrandsModule } from './brands/brands.module';
+import { NatsModule } from './transports/nats.module';
+import { ClientsModule } from './clients/clients.module';
+import { ProductsModule } from './products/products.module';
+import { ProvidersModule } from './providers/providers.module';
 import { CompaniesModule } from './companies/companies.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [ClientsModule, NatsModule, ProductsModule, BrandsModule, CategoriesModule, ProvidersModule, AuthModule, CompaniesModule ],
