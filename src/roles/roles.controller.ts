fb80c04ea3e8afa6c6aaebc9da42386ef6987908
Mattias Duarte <duartemattias4@gmail.com>
2026-02-19 15:00:21 -0400

    feat: create roles controller

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -8,28 +8,28 @@ import { FilterPaginationDto } from 'src/common/dto/filter-pagination.dto';
 @Controller('roles')
 export class RolesController {
 
-  constructor(
-    @Inject(NATS_SERVICE) private readonly client: ClientProxy
-  ) { }
+    constructor(
+        @Inject(NATS_SERVICE) private readonly client: ClientProxy
+    ) { }
 
-  @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.client.send("createRole", createRoleDto)
-      .pipe(
-        catchError(error => {
-          throw new RpcException(error)
-        })
-      )
-  }
+    @Post()
+    create(@Body() createRoleDto: CreateRoleDto) {
+        return this.client.send("createRole", createRoleDto)
+            .pipe(
+                catchError(error => {
+                    throw new RpcException(error)
+                })
+            )
+    }
 
-  @Get()
-  findAll(@Query() filterPaginationDto: FilterPaginationDto) {
-    return this.client.send("findAllRoles", filterPaginationDto)
-      .pipe(
-        catchError(error => { 
-          throw new RpcException(error)
-        })
-      )
+    @Get()
+    findAll(@Query() filterPaginationDto: FilterPaginationDto) {
+        return this.client.send("findAllRoles", filterPaginationDto)
+            .pipe(
+                catchError(error => {
+                    throw new RpcException(error)
+                })
+            )
 
-  }
+    }
 }
